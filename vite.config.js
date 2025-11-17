7f3f5740ffe4d0e5d9fb4f3a0489dcaed96cfbf9
leonardoleite.avanade <leonardoleite.avanade@natura.net>
2025-11-17 18:29:57 -0300

    css

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss' // <-- 1. Importe o tailwind
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: { // <-- 2. Adicione este bloco 'css'
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
 })
